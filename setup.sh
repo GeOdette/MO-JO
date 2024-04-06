76d457782cc2aaf5d53853bcdae6abb7f4bd69bf
GeOdette <steveodettegeorge@gmail.com>
2024-04-07 02:49:14 +0300

    updated readme, updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
-conda install -n base -c conda-forge mamba
+conda install -c conda-forge mamba -y
 
-mamba create -c conda-forge -c bioconda -n NGS_P snakemake
-
-conda activate NGS_P
+mamba install -c conda-forge -c bioconda snakemake -y
 
 conda install bioconda::fastqc -y
 
-mamba install fastp -y
+mamba install -c bioconda fastp -y
 
 conda install bioconda::bwa -y
 
-conda install bioconda::samtools -y
+conda install  bioconda::samtools -y
 
 conda install bioconda::bcftools -y
 
-conda install bioconda::multiqc -y
+conda install mojo bioconda::multiqc -y
 
 sudo apt-get install pandoc
 
-# Installing additional dependencies
 pip install snakemake-wrapper-utils
 
-echo "Installation complete. Activate the conda environment with 'conda activate NGS_p' before running the pipeline."
+echo "Installation complete."
